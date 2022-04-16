73ae60818fe53d75d6b785765e7f6cb9a16a1dc4
Italo Garcia <italo@landgorilla.com>
2022-04-15 23:48:51 -0500

    refix method save in user

--- controllers/UsuarioController.php
+++ controllers/UsuarioController.php
@@ -10,19 +10,40 @@ class usuarioController{
     public function registro(){
         require_once 'views/usuario/registro.php';
     }
+    public function validator($valor){
+         return $valor = isset($valor) ? $valor : false;
+    }
     public function save(){
+        
         if(isset($_POST)){
-            $usuario = new Usuario;
-            $usuario->setNombres($_POST['nombre']);
-            $usuario->setApellidos($_POST['apellidos']);
-            $usuario->setEmail($_POST['email']);
-            $usuario->setPassword($_POST['contrasena']);
-            $save = $usuario->save();
-            echo $save ? $_SESSION['register'] = "complete" : $_SESSION['register'] = "failed";
+            $nombre = $this->validator ($_POST['nombre']);
+            $apellidos = $this->validator ($_POST['apellidos']);
+            $email = $this->validator ($_POST['email']);
+            $contrasena = $this->validator ($_POST['contrasena']);
+            if($nombre && $apellidos && $email && $contrasena){
+                $usuario = new Usuario;
+                $usuario->setNombres($nombre);
+                $usuario->setApellidos($apellidos);
+                $usuario->setEmail($email);
+                $usuario->setPassword($contrasena);
+                $save = $usuario->save();
+                echo $save ? $_SESSION['register'] = "complete" : $_SESSION['register'] = "failed";
+            }else{
+                $_SESSION['register'] = "failed";
+            
+            }
         }else{
-            $_SESSION['register'] = "complete";
+            $_SESSION['register'] = "failed";
         }
         header("Location: ".base_url.'usuario/registro');
     }
+
+    public function login(){
+
+
+
+        
+    }
+
 
 }
--- views/usuario/registro.php
+++ views/usuario/registro.php
@@ -1,21 +1,26 @@
 <h1>Registrarse</h1>
-<?php if(isset($_SESSION['register'])): ?>
-        <strong>Registro completado correctamente</strong>
-<?php else: ?>
-    <strong>Registro fallido</strong>
+<?php if(isset($_SESSION['register']) && $_SESSION['register'] == 'complete'): ?>
+        <strong class="alert_green">Registro completado correctamente</strong>
+<?php elseif(isset($_SESSION['register']) && $_SESSION['register'] == 'failed'): ?>
+    <strong class="alert_red">Registro fallido</strong>
 <?php endif; ?>
+<?php Utils::deleteSession('register'); ?>
 <form action="<?=base_url?>usuario/save" method="POST">
     <label for="nombre">Nombre</label>
-    <input type="text" name="nombre" required/>
+    <!-- <input type="text" name="nombre" required/> -->
+    <input type="text" name="nombre" />
 
     <label for="apellidos">Apellidos</label>
-    <input type="text" name="apellidos" required/>
+    <!-- <input type="text" name="apellidos" required/> -->
+    <input type="text" name="apellidos" />
 
     <label for="email">Email</label>
-    <input type="email" name="email" required/>
-
+    <!-- <input type="email" name="email" required/> -->
+    <input type="email" name="email" />
+    
     <label for="contrasena">Contraseña</label>
-    <input type="password" name="contrasena" required/>
+    <!-- <input type="password" name="contrasena" required/> -->
+    <input type="password" name="contrasena" />
 
     <input type="submit" value="Registrarse"/>
 </form>
